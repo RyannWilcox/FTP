d9abd87d8c0973cc61760854dbeda2e36ac2a263
RyannWilcox <ryanwilcox1@yahoo.com>
2014-05-19 14:30:56 -0400

    not working client
    Client does not connect to the server for some reason.  Permission is
    denied.

--- client.py
+++ client.py
@@ -1,3 +1,19 @@
 #CLIENT
 
+from ftplib import FTP
 
+'''this does not work yet....'''
+
+def connectToFtp():
+    #Get all information from the user.
+    servername = raw_input("input the servers name: ");
+    username = raw_input("input username..");
+    password = raw_input("input password..");
+    
+    #attempt login to FTP
+    ftp = FTP(servername);
+    ftp.login(username,password);
+
+
+connectToFtp(); # try and log in.
+    
