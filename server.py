b892c85a8adfb4d83a1afed6cba20e028e480917
RyannWilcox <ryanwilcox1@yahoo.com>
2014-05-19 14:30:22 -0400

    start of ftp server
    This is the start of the ftp server.  I few important parts have to be
    added.  For example: security for passwords, Data caps, etc..

--- server.py
+++ server.py
@@ -1,1 +1,37 @@
 #SERVER
+import os
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+
+def createServer(ipaddr,username,password):
+    authorizer = DummyAuthorizer();
+    # create a user
+    authorizer.add_user(username, password, "?", perm="elradfmwM",
+    msg_login = "You are logged in.", msg_quit = "bye.");
+    
+    #anon user
+    authorizer.add_anonymous("?")
+    
+    handler = FTPHandler;
+    handler.authorizer = authorizer;
+    handler.banner = "FileTransferProtocol";
+    
+    server = FTPServer((ipaddr,21), handler);
+    
+    #limits on connections to ftp server
+    server.max_cons = 256;
+    server.max_cons_per_ip = 5;
+    server.serve_forever();
+
+
+
+createServer("xxx.xxx.x.xxx","user","password");    
+        
+
+
+
+
+
+
