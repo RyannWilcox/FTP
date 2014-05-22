52d4b9262a7c51b79dbab7334b4a3cd26952064f
RyannWilcox <ryanwilcox1@yahoo.com>
2014-05-22 12:36:24 -0400

    turned the server into a class
    Gave it a constructor and functions.  Also started using the import
    socket to get the local host ip, instead of typing out my ip address

--- server.py
+++ server.py
@@ -1,33 +1,44 @@
 #SERVER
 import os
+import socket
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
-
-def createServer(ipaddr,username,password):
-    authorizer = DummyAuthorizer();
-    # create a user
-    authorizer.add_user(username, password, "/users/ryanwilcox/server", perm="elradfmwM",
-    msg_login = "You are logged in.", msg_quit = "bye.");
+class Server:
     
-    #anon user
-    authorizer.add_anonymous("/users/ryanwilcox/server");
+    def __init__(self,startMessage):
+        self.startMessage = startMessage;
+        print self.startMessage;
     
-    handler = FTPHandler;
-    handler.authorizer = authorizer;
-    handler.banner = "FileTransferProtocol";
     
-    server = FTPServer((ipaddr,6066), handler);
     
-    #limits on connections to ftp server
-    server.max_cons = 256;
-    server.max_cons_per_ip = 5;
-    server.serve_forever();
-#END OF FUNCTION
+    def createServer(self,ipaddr,username,password):
+        authorizer = DummyAuthorizer();
+        # create a user
+        authorizer.add_user(username, password, "/users/ryanwilcox/server", perm="elradfmwM",
+        msg_login = "You are logged in.", msg_quit = "bye.");
+    
+        #anon user
+        authorizer.add_anonymous("/users/ryanwilcox/server");
+    
+        handler = FTPHandler;
+        handler.authorizer = authorizer;
+        handler.banner = "FileTransferProtocol";
+    
+        server = FTPServer((ipaddr,6066), handler);
+    
+        #limits on connections to ftp server
+        server.max_cons = 256;
+        server.max_cons_per_ip = 5;
+        server.serve_forever();
+        #END OF FUNCTION
  
 
-createServer("192.168.1.119","user","password");    
+serv = Server("Start the Server");
+servername = socket.gethostname();
+serv.createServer(servername,"user","password");    
+
         
 
 
